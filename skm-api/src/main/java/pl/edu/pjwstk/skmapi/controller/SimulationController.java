64367d326719d3ce86ca31db7a88847adf2703bb
maciek_kucharski <56729054+Maciekkucharski@users.noreply.github.com>
2020-12-25 21:38:21 +0100

    simulation controller etst

--- skm-api/src/main/java/pl/edu/pjwstk/skmapi/controller/SimulationController.java
+++ skm-api/src/main/java/pl/edu/pjwstk/skmapi/controller/SimulationController.java
@@ -1,5 +1,7 @@
 package pl.edu.pjwstk.skmapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,12 @@ public class SimulationController {
     }
 
     @PostMapping
-    public void move(){
-        simulationService.move();
+    public ResponseEntity<Object> move(){
+        try {
+            simulationService.move();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- skm-api/src/test/java/pl/edu/pjwstk/skmapi/controllerTests/SimulationControllerTest.java
+++ skm-api/src/test/java/pl/edu/pjwstk/skmapi/controllerTests/SimulationControllerTest.java
@@ -1,2 +1,39 @@
-package pl.edu.pjwstk.skmapi.controllerTests;public class SimulationControllerTest {
+package pl.edu.pjwstk.skmapi.controllerTests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.edu.pjwstk.skmapi.controller.SimulationController;
+import pl.edu.pjwstk.skmapi.services.SimulationService;
+
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SimulationController.class)
+public class SimulationControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    SimulationService service;
+
+    @Test
+    public void moveBadRequest() throws Exception {
+        doThrow(new RuntimeException()).when(service).move();
+        mockMvc.perform(MockMvcRequestBuilders.post("/move")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void moveIsOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/move")).andExpect(status().isOk());
+    }
+
 }
